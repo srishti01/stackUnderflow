f13396934768063c02500e356ff40ca60b7b2d54
Yash <yash.yg.garg111@gmail.com>
2020-10-05 15:36:09 +0530

    Added Main Activity with only bottom navigation bar

--- app/src/main/java/com/example/stackunderflow/NotificationActivity.java
+++ app/src/main/java/com/example/stackunderflow/NotificationActivity.java
@@ -3,6 +3,7 @@ package com.example.stackunderflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        //test
     }
 }
