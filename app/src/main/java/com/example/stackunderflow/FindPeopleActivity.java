112af725aa19bbca9b29b3b78605708d8b5566f2
sonalprd <sonal.prasad.01@gmail.com>
2020-10-06 19:24:10 +0530

    new xml activities created

--- app/src/main/java/com/example/stackunderflow/FindPeopleActivity.java
+++ app/src/main/java/com/example/stackunderflow/FindPeopleActivity.java
@@ -5,42 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class NotificationActivity extends AppCompatActivity {
+public class FindPeopleActivity extends AppCompatActivity {
 
-    private  RecyclerView NotificationList;
+    private RecyclerView findPeopleList;
+    private EditText searchUserText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
-        //test
+        setContentView(R.layout.activity_find_people);
 
-        NotificationList=findViewById(R.id.notification_list);
-        NotificationList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        findPeopleList=findViewById(R.id.find_people_list);
+        searchUserText=findViewById(R.id.search_user_text);
+        findPeopleList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
     }
 
     public static class notificationViewHolder extends RecyclerView.ViewHolder {
 
         TextView userNameText;
-        Button acceptBtn,cancelBtn;
+        Button videoCallBtn;
         ImageView profileImage;
-        RelativeLayout CardView;
+        RelativeLayout CardView1;
 
 
         public notificationViewHolder(@NonNull View itemView) {
             super(itemView);
 
             userNameText=itemView.findViewById(R.id.name_notification);
-            acceptBtn=itemView.findViewById(R.id.request_accept_btn);
-            cancelBtn=itemView.findViewById(R.id.request_cancel_btn);
-            CardView=itemView.findViewById(R.id.card_view);
+            videoCallBtn=itemView.findViewById(R.id.request_accept_btn);
+            CardView1=itemView.findViewById(R.id.card_view1);
             profileImage=itemView.findViewById(R.id.image_notification);
 
         }
