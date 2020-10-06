9fbb9874db628359b4d6463b9c406e5ee716db22
Lenovo <singhsrishti4321@gmail.com>
2020-10-06 11:39:09 +0530

    current status

--- app/src/main/java/com/example/stackunderflow/MainActivity.java
+++ app/src/main/java/com/example/stackunderflow/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.stackunderflow;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+        navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_home:{
+                    //As we are originally in the main activity so the Intent will just Refresh the mainActivity
+                    Intent mainIntent = new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(mainIntent);
+                    break;
+                }
+                case R.id.navigation_settings:{
+                    //Setting Intent to show what would happen when notification icon is pressed
+                    Intent settingsIntent = new Intent(MainActivity.this,SettingsActivity.class);
+                    startActivity(settingsIntent);
+                    break;
+                }
+                case R.id.navigation_notifications:{
+                    Intent notificationIntent = new Intent(MainActivity.this,NotificationActivity.class);
+                    startActivity(notificationIntent);
+                    break;
+                }
+//                case R.id.navigation_logout:{
+//                    Intent logoutIntent = new Intent(MainActivity.this,RegistrationActivity.class);//Registration activity will be given by ss
+//                    startActivity(logoutIntent);
+//                    finish();
+//                    break;
+//                }
+            }
+            return true;
+        }
+    };
+
 }
